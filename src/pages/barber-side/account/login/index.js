6402e6f9b9d58cf67c465110497ee50075319f66
Guilherme Victor <guilhermevr.figueiredo@outlook.com>
2024-08-07 21:46:38 -0300

    feat: adiciona funcionalidade de login a barbearia

--- src/pages/barber-side/account/login/index.js
+++ src/pages/barber-side/account/login/index.js
@@ -1,10 +1,33 @@
-
-
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from'./login.module.css'
+import { Post } from '../../../../core/service/post';
 
 
 export default function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginBarber = {
+    email: email,
+    password: password,
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+ 
+    Post('http://localhost:8080/cutandtrim/signup', loginBarber)
+      .then(jBody => {
+        console.log(jBody)
+        if(jBody.barberShopID) {    
+          localStorage.setItem("barberShopID", jBody.barberShopID);
+          window.location.href = 'http://localhost:3000/barber/service/list';
+        }
+      }).catch(error => {
+        console.error('Erro:', error);
+      });
+  }
   return (
 
 
@@ -23,12 +46,12 @@ export default function Login() {
 
       <div className={styles.login_container}>
 
-        <form className={styles.form_container}>
+        <form className={styles.form_container} onClick={handleSubmit}>
           <div className={styles.input_body}>
             <label>Email:</label>
             <div className={styles.input_container}>
               <i className="fi fi-sr-envelope"></i>
-              <input type="text" name="" id="" />
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
           </div>
 
@@ -36,16 +59,14 @@ export default function Login() {
             <label>Password:</label>
             <div className={styles.input_container}>
               <i className="fi fi-sr-shield"></i>
-              <input type="password" />
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
               <i className="fi fi-sr-eye"></i>
             </div>
           </div>
+          <footer className={styles.footer}>
+          <button className={styles.btn_continue}>Continue</button>
+          </footer>
         </form>
-
-        <footer className={styles.footer}>
-          <Link to={'/barber/account/barber-shop'}><button className='btn_continue'>Continue</button></Link>
-          <p>Already have a account? LINK</p>
-        </footer>
       </div>
     </section >
   );
